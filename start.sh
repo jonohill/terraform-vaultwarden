7ae9977584f0448ad3e4ec79447d7e2dc165b92c
Jono Hill <jono@hillnz.com>
2023-01-28 09:59:00 +1300

    tweak dockerfile

--- start.sh
+++ start.sh
@@ -6,14 +6,14 @@ echo "Writing keys"
 echo "$RSA_KEY" > /tmp/rsa_key.pem
 echo "$RSA_KEY_PUB" > /tmp/rsa_key.pub.pem
 
-RUN_TIMEOUT=${RUN_TIMEOUT:-600}
+RUN_TIMEOUT=${RUN_TIMEOUT:-300}
 
 timeout_pid=
 timeout() {
     if [ -n "$timeout_pid" ]; then
         kill -s SIGTERM "$timeout_pid"
     fi
-    (sleep "$RUN_TIMEOUT" && echo "No output for $RUN_TIMEOUT seconds, exiting" && kill -s SIGTERM "$(cat /tmp/vaultwarden.pid)") &
+    (sleep "$RUN_TIMEOUT" && echo "No output for $RUN_TIMEOUT seconds" && kill -s SIGTERM "$(cat /tmp/vaultwarden.pid)") &
     timeout_pid=$!
 }
 
@@ -21,3 +21,6 @@ while read -r line; do
     echo "$line"
     timeout
 done < <(/vaultwarden "$@" & echo "$!" >/tmp/vaultwarden.pid; wait "$!" || true)
+
+echo "Exiting"
+exit 0
